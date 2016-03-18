a793c3c6c447fe40dae35109991baa95988501ac
XMG <XMG@163.com>
2016-03-18 11:56:55 +0800

    6666

--- BuDeJie/Classes/Essence(精华)/Controller/XMGEssenceViewController.m
+++ BuDeJie/Classes/Essence(精华)/Controller/XMGEssenceViewController.m
@@ -7,16 +7,17 @@
 //
 
 #import "XMGEssenceViewController.h"
+#import "XMGTitleButton.h"
 
-// UIBarButtonItem:描述按钮具体的内容
-// UINavigationItem:设置导航条上内容(左边,右边,中间)
-// tabBarItem: 设置tabBar上按钮内容(tabBarButton)
 @interface XMGEssenceViewController ()
-
+/** 标题栏 */
+@property (nonatomic, weak) UIView *titlesView;
+/** 上一次点击的标题按钮 */
+@property (nonatomic, weak) XMGTitleButton *previousClickedTitleButton;
 @end
 
 @implementation XMGEssenceViewController
-
+#pragma mark - 初始化
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
@@ -31,34 +32,10 @@
     [self setupTitlesView];
 }
 
-/**
- *  scrollView
- */
-- (void)setupScrollView
-{
-    UIScrollView *scrollView = [[UIScrollView alloc] init];
-    scrollView.backgroundColor = [UIColor redColor];
-    scrollView.frame = self.view.bounds;
-    [self.view addSubview:scrollView];
-}
 
 /**
- *  标题栏
+ *  设置导航条
  */
-- (void)setupTitlesView
-{
-    UIView *titlesView = [[UIView alloc] init];
-    // 设置半透明背景色
-    titlesView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
-//    titlesView.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.5];
-//    titlesView.backgroundColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.5];
-    // 子控件会继承父控件设置的alpha透明度
-//    titlesView.alpha = 0.5;
-    titlesView.frame = CGRectMake(0, 64, self.view.xmg_width, 35);
-    [self.view addSubview:titlesView];
-}
-
-#pragma mark - 设置导航条
 - (void)setupNavBar
 {
     // 左边按钮
@@ -72,9 +49,102 @@
     self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"MainTitle"]];
 }
 
+/**
+ *  scrollView
+ */
+- (void)setupScrollView
+{
+    UIScrollView *scrollView = [[UIScrollView alloc] init];
+    scrollView.backgroundColor = [UIColor blueColor];
+    scrollView.frame = self.view.bounds;
+    [self.view addSubview:scrollView];
+    /*
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(0, 0, 100, 50);
+    [btn setTitle:@"按钮" forState:UIControlStateNormal];
+    //    [btn setBackgroundImage:[UIImage imageNamed:@"mine-my-post"] forState:UIControlStateNormal];
+    // 加载bundle里面的图片需要加上bundle的文件名, 格式: bundle文件名/图片名
+//    [btn setImage:[UIImage imageNamed:@"SVProgressHUD.bundle/error"] forState:UIControlStateNormal];
+    [scrollView addSubview:btn];
+     */
+}
+
+/**
+ *  标题栏
+ */
+- (void)setupTitlesView
+{
+    UIView *titlesView = [[UIView alloc] init];
+    titlesView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
+    titlesView.frame = CGRectMake(0, 64, self.view.xmg_width, 35);
+    [self.view addSubview:titlesView];
+    self.titlesView = titlesView;
+    
+    // 标题栏按钮
+    [self setupTitleButtons];
+    
+    // 标题下划线
+//    [self setupTitleUnderline];
+}
+
+/**
+ *  标题栏按钮
+ */
+- (void)setupTitleButtons
+{
+    // 文字
+    NSArray *titles = @[@"全部", @"视频", @"声音", @"图片", @"段子"];
+    NSUInteger count = titles.count;
+    
+    // 标题按钮的尺寸
+    CGFloat titleButtonW = self.titlesView.xmg_width / count;
+    CGFloat titleButtonH = self.titlesView.xmg_height;
+    
+    // 创建5个标题按钮
+    for (NSUInteger i = 0; i < count; i++) {
+        XMGTitleButton *titleButton = [[XMGTitleButton alloc] init];
+        [titleButton addTarget:self action:@selector(titleButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self.titlesView addSubview:titleButton];
+        // frame
+        titleButton.frame = CGRectMake(i * titleButtonW, 0, titleButtonW, titleButtonH);
+        // 文字
+        [titleButton setTitle:titles[i] forState:UIControlStateNormal];
+        [titleButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+        [titleButton setTitleColor:[UIColor redColor] forState:UIControlStateSelected];
+//        [titleButton setTitleColor:[UIColor redColor] forState:UIControlStateDisabled];
+    }
+}
+
+#pragma mark - 监听
+/**
+ *  点击标题按钮
+ */
+- (void)titleButtonClick:(XMGTitleButton *)titleButton
+{
+    self.previousClickedTitleButton.selected = NO;
+    titleButton.selected = YES;
+    self.previousClickedTitleButton = titleButton;
+}
+
+//- (void)titleButtonClick:(XMGTitleButton *)titleButton
+//{
+//    self.previousClickedTitleButton.enabled = YES;
+//    titleButton.enabled = NO;
+//    self.previousClickedTitleButton = titleButton;
+//    
+//    XMGFunc
+//}
+
+//- (void)titleButtonClick:(XMGTitleButton *)titleButton
+//{
+//    [self.previousClickedTitleButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
+//    [titleButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+//    self.previousClickedTitleButton = titleButton;
+//}
+
 - (void)game
 {
-    XMGFunc;
+    XMGFunc
 }
 
 @end
---
+++ BuDeJie/Classes/Essence(精华)/View/XMGTitleButton.m
@@ -0,0 +1,17 @@
+//
+//  XMGTitleButton.m
+//  BuDeJie
+//
+//  Created by xiaomage on 16/3/18.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import "XMGTitleButton.h"
+
+@implementation XMGTitleButton
+
+- (void)setHighlighted:(BOOL)highlighted
+{ // 只要重写了这个方法，按钮就无法进入highlighted状态
+    
+}
+@end
