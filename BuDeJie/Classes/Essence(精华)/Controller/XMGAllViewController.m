b199bc321c8011cf45c88e0b02f996377c5a4ba4
XMG <XMG@163.com>
2016-03-22 10:48:35 +0800

    6666666

--- BuDeJie/Classes/Essence(精华)/Controller/XMGAllViewController.m
+++ BuDeJie/Classes/Essence(精华)/Controller/XMGAllViewController.m
@@ -12,11 +12,18 @@
 /** 数据量 */
 @property (nonatomic, assign) NSInteger dataCount;
 
+/** 下拉刷新控件 */
+@property (nonatomic, weak) UIView *header;
+/** 下拉刷新控件里面的文字 */
+@property (nonatomic, weak) UILabel *headerLabel;
+/** 下拉刷新控件是否正在刷新 */
+@property (nonatomic, assign, getter=isHeaderRefreshing) BOOL headerRefreshing;
+
 /** 上拉刷新控件 */
 @property (nonatomic, weak) UIView *footer;
 /** 上拉刷新控件里面的文字 */
 @property (nonatomic, weak) UILabel *footerLabel;
-/** 上拉刷新控件时候正在刷新 */
+/** 上拉刷新控件是否正在刷新 */
 @property (nonatomic, assign, getter=isFooterRefreshing) BOOL footerRefreshing;
 @end
 
@@ -24,8 +31,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    self.dataCount = 10;
     
     self.view.backgroundColor = XMGRandomColor;
     
@@ -45,6 +50,34 @@
 
 - (void)setupRefresh
 {
+    // 广告条
+    UILabel *label = [[UILabel alloc] init];
+    label.backgroundColor = [UIColor blackColor];
+    label.frame = CGRectMake(0, 0, 0, 30);
+    label.textColor = [UIColor whiteColor];
+    label.text = @"广告";
+    label.textAlignment = NSTextAlignmentCenter;
+    self.tableView.tableHeaderView = label;
+    
+    // header
+    UIView *header = [[UIView alloc] init];
+    header.frame = CGRectMake(0, - 50, self.tableView.xmg_width, 50);
+    self.header = header;
+    [self.tableView addSubview:header];
+    
+    UILabel *headerLabel = [[UILabel alloc] init];
+    headerLabel.frame = header.bounds;
+    headerLabel.backgroundColor = [UIColor redColor];
+    headerLabel.text = @"下拉可以刷新";
+    headerLabel.textColor = [UIColor whiteColor];
+    headerLabel.textAlignment = NSTextAlignmentCenter;
+    [header addSubview:headerLabel];
+    self.headerLabel = headerLabel;
+    
+    // 让header自动进入刷新
+    [self headerBeginRefreshing];
+    
+    // footer
     UIView *footer = [[UIView alloc] init];
     footer.frame = CGRectMake(0, 0, self.tableView.xmg_width, 35);
     self.footer = footer;
@@ -73,7 +106,8 @@
     // 显示在正中间的不是AllViewController
     if (self.tableView.scrollsToTop == NO) return;
     
-    XMGLog(@"%@ - 刷新数据", self.class)
+    // 进入下拉刷新
+    [self headerBeginRefreshing];
 }
 
 /**
@@ -105,37 +139,151 @@
 }
 
 #pragma mark - 代理方法
+/**
+ *  用户松开scrollView时调用
+ */
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    CGFloat offsetY = - (self.tableView.contentInset.top + self.header.xmg_height);
+    if (self.tableView.contentOffset.y <= offsetY) { // header已经完全出现
+        [self headerBeginRefreshing];
+    }
+}
+
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    // 处理header
+    [self dealHeader];
+    
+    // 处理footer
+    [self dealFooter];
+}
+
+/**
+ *  处理header
+ */
+- (void)dealHeader
+{
+    // 如果正在下拉刷新，直接返回
+    if (self.isHeaderRefreshing) return;
+    
+    // 当scrollView的偏移量y值 <= offsetY时，代表header已经完全出现
+    CGFloat offsetY = - (self.tableView.contentInset.top + self.header.xmg_height);
+    if (self.tableView.contentOffset.y <= offsetY) { // header已经完全出现
+        self.headerLabel.text = @"松开立即刷新";
+        self.headerLabel.backgroundColor = [UIColor grayColor];
+    } else {
+        self.headerLabel.text = @"下拉可以刷新";
+        self.headerLabel.backgroundColor = [UIColor redColor];
+    }
+}
+
+/**
+ *  处理footer
+ */
+- (void)dealFooter
 {
     // 还没有任何内容的时候，不需要判断
     if (self.tableView.contentSize.height == 0) return;
     
-    // 如果正在刷新，直接返回
-    if (self.isFooterRefreshing) return;
-    
     // 当scrollView的偏移量y值 >= offsetY时，代表footer已经完全出现
     CGFloat ofsetY = self.tableView.contentSize.height + self.tableView.contentInset.bottom - self.tableView.xmg_height;
-    
-    if (self.tableView.contentOffset.y >= ofsetY) {
-        // 进入刷新状态
-        self.footerRefreshing = YES;
-        self.footerLabel.text = @"正在加载更多数据...";
-        self.footerLabel.backgroundColor = [UIColor blueColor];
-        
-        // 发送请求给服务器
-        XMGLog(@"发送请求给服务器 - 加载更多数据")
-        
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            // 服务器请求回来了
-            self.dataCount += 5;
-            [self.tableView reloadData];
-            
-            // 结束刷新
-            self.footerRefreshing = NO;
-            self.footerLabel.text = @"上拉可以加载更多";
-            self.footerLabel.backgroundColor = [UIColor redColor];
-        });
+    if (self.tableView.contentOffset.y >= ofsetY
+        && self.tableView.contentOffset.y > - (self.tableView.contentInset.top)) { // footer完全出现，并且是往上拖拽
+        [self footerBeginRefreshing];
     }
+}
+
+#pragma mark - 数据处理
+/**
+ *  发送请求给服务器，下拉刷新数据
+ */
+- (void)loadNewData
+{
+    XMGLog(@"发送请求给服务器，下拉刷新数据")
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // 服务器的数据回来了
+        self.dataCount = 20;
+        [self.tableView reloadData];
+        
+        // 结束刷新
+        [self headerEndRefreshing];
+    });
+}
+
+/**
+ *  发送请求给服务器，上拉加载更多数据
+ */
+- (void)loadMoreData
+{
+    XMGLog(@"发送请求给服务器 - 加载更多数据")
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // 服务器请求回来了
+        self.dataCount += 5;
+        [self.tableView reloadData];
+        
+        // 结束刷新
+        [self footerEndRefreshing];
+    });
+}
+
+#pragma mark - header
+- (void)headerBeginRefreshing
+{
+    // 如果正在下拉刷新，直接返回
+    if (self.isHeaderRefreshing) return;
+    
+    // 进入下拉刷新状态
+    self.headerLabel.text = @"正在刷新数据...";
+    self.headerLabel.backgroundColor = [UIColor blueColor];
+    self.headerRefreshing = YES;
+    // 增加内边距
+    [UIView animateWithDuration:0.25 animations:^{
+        UIEdgeInsets inset = self.tableView.contentInset;
+        inset.top += self.header.xmg_height;
+        self.tableView.contentInset = inset;
+        
+        // 修改偏移量
+        self.tableView.contentOffset = CGPointMake(self.tableView.contentOffset.x,  - inset.top);
+    }];
+    
+    // 发送请求给服务器，下拉刷新数据
+    [self loadNewData];
+}
+
+- (void)headerEndRefreshing
+{
+    self.headerRefreshing = NO;
+    // 减小内边距
+    [UIView animateWithDuration:0.25 animations:^{
+        UIEdgeInsets inset = self.tableView.contentInset;
+        inset.top -= self.header.xmg_height;
+        self.tableView.contentInset = inset;
+    }];
+}
+
+#pragma mark - footer
+- (void)footerBeginRefreshing
+{
+    // 如果正在上拉刷新，直接返回
+    if (self.isFooterRefreshing) return;
+    
+    // 进入刷新状态
+    self.footerRefreshing = YES;
+    self.footerLabel.text = @"正在加载更多数据...";
+    self.footerLabel.backgroundColor = [UIColor blueColor];
+    
+    // 发送请求给服务器，上拉加载更多数据
+    [self loadMoreData];
+}
+
+- (void)footerEndRefreshing
+{
+    self.footerRefreshing = NO;
+    self.footerLabel.text = @"上拉可以加载更多";
+    self.footerLabel.backgroundColor = [UIColor redColor];
 }
 
 @end
